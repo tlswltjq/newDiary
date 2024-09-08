ac81091c06dd9dc9703beb913a8bbc5f99c256eb
rake_13@naver.com <wltjq1203@icloud.com>
2024-09-08 20:42:35 +0900

    테스트 환경에서 롬복을 사용하기 위해 testCompileOnly, testAnnotationProcessor 를 추가

--- src/test/java/org/example/newdiary/NewDiaryApplicationTests.java
+++ src/test/java/org/example/newdiary/NewDiaryApplicationTests.java
@@ -1,13 +1,26 @@
 package org.example.newdiary;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class NewDiaryApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @BeforeEach
+    void before() {
+        log.info("모든 테스트 전 실행");
     }
 
+    @AfterEach
+    void after(){
+        log.info("모든 테스트 후 실행");
+    }
+    @Test
+    void testTesting(){
+        log.info("테스트 테스트");
+    }
 }
