7589fe9d033a5cf70d5cdf5393cb3cbe89db75f4
rake_13@naver.com <wltjq1203@icloud.com>
2024-09-17 20:41:51 +1000

    초기화

--- src/main/java/org/example/newdiary/repository/TodoListRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.newdiary.repository;
-
-import org.example.newdiary.entity.TodoList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoListRepository extends JpaRepository<TodoList, Integer> {
-}
