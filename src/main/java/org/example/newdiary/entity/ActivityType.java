79b8f08d036e4fb0a491ddbc8e40bc988a4ffb94
rake_13@naver.com <wltjq1203@icloud.com>
2024-10-08 21:33:31 +1100

    ActivityType 추가

--- src/main/java/org/example/newdiary/entity/ActivityType.java
+++ src/main/java/org/example/newdiary/entity/ActivityType.java
@@ -2,4 +2,5 @@ package org.example.newdiary.entity;
 
 public enum ActivityType {
     todo_done, todo_unDone, todo_add, todo_update, todo_delete,
+    todoList_create, todoList_delete, todoList_rename
 }
