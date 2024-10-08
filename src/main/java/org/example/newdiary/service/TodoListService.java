19167eb1e0a013ca07705d603c01951a63fc1c80
rake_13@naver.com <wltjq1203@icloud.com>
2024-10-08 21:58:47 +1100

    작업에 맞는 TodoList와 ActivityType을 전달해 NewActivityEvent 이벤트 발행

--- src/main/java/org/example/newdiary/service/TodoListService.java
+++ src/main/java/org/example/newdiary/service/TodoListService.java
@@ -1,22 +1,29 @@
 package org.example.newdiary.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.newdiary.entity.ActivityType;
 import org.example.newdiary.entity.TodoList;
+import org.example.newdiary.event.NewActivityEvent;
 import org.example.newdiary.repository.TodoListRepository;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TodoListService {
     private final TodoListRepository todoListRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     public TodoList createTodoList(String name){
         TodoList todoList = TodoList.builder()
                 .name(name)
                 .build();
-        return todoListRepository.save(todoList);
+        TodoList savedTodoList = todoListRepository.save(todoList);
+        eventPublisher.publishEvent(new NewActivityEvent<>(this, savedTodoList, ActivityType.todoList_create));
+        return savedTodoList;
     }
 
     public TodoList getTodoList(Long listId) {
@@ -26,10 +33,13 @@ public class TodoListService {
     public TodoList updateTodoListName(String newName, Long id) {
         TodoList todoList = getTodoList(id);
         TodoList updated = todoList.updateName(newName);
+        eventPublisher.publishEvent(new NewActivityEvent<>(this, updated, ActivityType.todoList_rename));
         return todoListRepository.save(updated);
     }
 
     public void deleteTodoList(Long id) {
-        todoListRepository.deleteById(id);
+        TodoList todoList = getTodoList(id);
+        eventPublisher.publishEvent(new NewActivityEvent<>(this, todoList, ActivityType.todoList_delete));
+        todoListRepository.delete(todoList);
     }
 }
