def3e59bebab5544370932e3b4fce31caef815e7
rake_13@naver.com <wltjq1203@icloud.com>
2024-10-08 22:05:20 +1100

    작업에 맞는 Todo와 ActivityType을 전달해 NewActivityEvent 이벤트 발행

--- src/main/java/org/example/newdiary/service/TodoService.java
+++ src/main/java/org/example/newdiary/service/TodoService.java
@@ -2,9 +2,12 @@ package org.example.newdiary.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example.newdiary.entity.ActivityType;
 import org.example.newdiary.entity.Todo;
 import org.example.newdiary.entity.TodoList;
+import org.example.newdiary.event.NewActivityEvent;
 import org.example.newdiary.repository.TodoRepository;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -14,6 +17,7 @@ import java.util.NoSuchElementException;
 public class TodoService {
     private final TodoRepository todoRepository;
     private final TodoListService todoListService;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Todo createTodo(String description, Long listId) {
         TodoList todoList = todoListService.getTodoList(listId);
@@ -22,6 +26,7 @@ public class TodoService {
                 .isDone(false)
                 .todoList(todoList)
                 .build();
+        eventPublisher.publishEvent(new NewActivityEvent<>(this, todo, ActivityType.todo_add));
         return todoRepository.save(todo);
     }
 
@@ -32,21 +37,26 @@ public class TodoService {
 
     public Todo doneTodo(Long todoId){
         Todo todo = getTodo(todoId);
+        eventPublisher.publishEvent(new NewActivityEvent<>(this, todo, ActivityType.todo_done));
         return todoRepository.save(todo.done());
     }
 
     public Todo unDoneTodo(Long todoId){
         Todo todo = getTodo(todoId);
+        eventPublisher.publishEvent(new NewActivityEvent<>(this, todo, ActivityType.todo_unDone));
         return todoRepository.save(todo.unDone());
     }
 
     public Todo updateTodo(Long todoId, String newDescription) {
         Todo todo = getTodo(todoId);
+        eventPublisher.publishEvent(new NewActivityEvent<>(this, todo, ActivityType.todo_update));
         return todoRepository.save(todo.updateDescription(newDescription));
     }
 
     @Transactional
     public void deleteTodo(Long todoId) {
-        todoRepository.deleteById(todoId);
+        Todo todo = getTodo(todoId);
+        eventPublisher.publishEvent(new NewActivityEvent<>(this, todo, ActivityType.todo_delete));
+        todoRepository.delete(todo);
     }
 }
