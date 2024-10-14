8d49150b5af3f096f30d0c8268b408a28a7e7055
rake_13@naver.com <wltjq1203@icloud.com>
2024-10-14 22:56:15 +1100

    최근 n개월의 Activity를 리스트로 반환하는 메서드 작성

--- src/main/java/org/example/newdiary/service/ActivityService.java
+++ src/main/java/org/example/newdiary/service/ActivityService.java
@@ -6,6 +6,8 @@ import org.example.newdiary.repository.ActivityRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +21,12 @@ public class ActivityService {
                 .timeStamp(LocalDateTime.now())
                 .build();
         return activityRepository.save(activity);
+    }
+
+    public List<Activity> getActivitiesWithinMonths(int months) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime startDate = now.minusMonths(months-1).with(TemporalAdjusters.firstDayOfMonth()).withHour(0).withMinute(0).withSecond(0).withNano(0);
+        LocalDateTime endDate = now.with(TemporalAdjusters.lastDayOfMonth()).withHour(23).withMinute(59).withSecond(59).withNano(999999999);
+        return activityRepository.findActivitiesWithinMonths(startDate, endDate);
     }
 }
