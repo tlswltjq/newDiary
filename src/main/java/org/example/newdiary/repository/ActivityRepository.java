cc54f0b799469d07b61605077bac5f4e306f9c60
rake_13@naver.com <wltjq1203@icloud.com>
2024-10-14 22:55:38 +1100

    JPQL을 이용한 메서드 작성

--- src/main/java/org/example/newdiary/repository/ActivityRepository.java
+++ src/main/java/org/example/newdiary/repository/ActivityRepository.java
@@ -2,8 +2,15 @@ package org.example.newdiary.repository;
 
 import org.example.newdiary.entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    @Query("SELECT a FROM Activity a WHERE a.timeStamp BETWEEN :startDate AND :endDate")
+    List<Activity> findActivitiesWithinMonths(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
